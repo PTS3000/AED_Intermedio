3bbf9738b7f7dbbb907b7905df48345be60e8745
Luís Parra <parra@MacBook-Pro-de-Luis.local>
2021-10-12 11:54:01 +0100

    message

--- Read.c
+++ Read.c
@@ -1,44 +1,64 @@
 #include "Proj_Int.h"
 
 /**
- * @brief 
+ * @brief Function that reads the labyrinth of from the given file and dinamically stores the information contained in it
  * 
  * @param fp_in 
- * @param labyrinth 
  * @param filename 
+ * @param test_mode 
+ * @return int** 
  */
 
-void Read_input_file(char *filename)
+int **Read_input_file(FILE *fp_in, int **labyrinth, char *filename)
 {
-    // int **traversed_path = NULL;
-    //int *size_traversed = NULL;
-    int x = 0, y = 0, v = 0, count = 0, P = 0, result = 0; //P= number of grey/black cells, (x,y) = grey/black cells coordinates, v=value of respective cell
-    int C = 0, L = 0, a = 0, b = 0, c = 0, d = 0;          // C=columns, L=lines, (a,b)=coordinates of the cell we want to analyse, P= number of grey/black cells
-    int **labyrinth = NULL;
-    char test_mode[3];
-    char *out_name = NULL; //name of the output file
+    
+    int **traversed_path = NULL;
+    int *size_traversed = NULL;
 
-    FILE *fp_in = NULL, *fp_out = NULL;
 
-    fp_in = open_inputfile(fp_in, filename);
 
-    out_name = allocate_outputname(out_name, filename);
-    fp_out = open_outputfile(fp_out, out_name);
-    while ((fscanf(fp_in, "%d %d", &L, &C)) == 2)
+
+    int x = 0, y = 0, v = 0, count = 0, P = 0, result = 0;      //P= number of grey/black cells, (x,y) = grey/black cells coordinates, v=value of respective cell
+    int C = 0, L = 0, a = 0, b = 0, c = 0, d = 0, num_char = 0; // C=columns, L=lines, (a,b)=coordinates of the cell we want to analyse, P= number of grey/black cells
+    char *test_mode = (char *)calloc(3, sizeof(char));
+
+    //meter numa funcao
+    num_char = find_last_period(filename);
+    char *name = (char *)calloc((num_char + 5), sizeof(char));
+    if (name == NULL)
+    {
+        fprintf(stderr, "Not enough memory available!\n");
+        exit(5);
+    }
+    strncpy(name, filename, num_char);
+    strcat(name, "sol1");
+
+    if ((fp_in = fopen(filename, "r")) == NULL) //inicio da leitura do ficheiro
+    {
+        printf("Error when reading the input file1.\n");
+        fclose(fp_in);
+        exit(EXIT_FAILURE);
+    }
+    while ((fscanf(fp_in, "%d %d", &L, &C)) != EOF)
     {
         if ((fscanf(fp_in, "%d %d %s", &a, &b, test_mode)) != 3)
-            error(fp_in);
-
+        {
+            printf("Error reading data from input file2.\n");
+            exit(0);
+        }
         if (strcmp(test_mode, "A6") == 0)
             if ((fscanf(fp_in, "%d %d", &c, &d)) != 2)
-                error(fp_in);
-
-        labyrinth = allocate_table(labyrinth, C, L); // Dynamic allocation of the main labyrinth
+            {
+                printf("Error reading data from input file3.\n");
+                exit(0);
+            }
+        // Dynamic allocation of the main labyrinth
+        labyrinth = allocate_table(labyrinth, C, L);
 
         if ((fscanf(fp_in, "%d", &P)) != 1)
         {
-            free_labyrinth(labyrinth, C);
-            error(fp_in);
+            printf("Error reading data from input file.\n");
+            exit(0);
         }
         while ((fscanf(fp_in, "%d %d %d", &x, &y, &v) == 3) && (count < P))
         {
@@ -46,21 +66,17 @@ void Read_input_file(char *filename)
             count++;
         }
         //print_table(labyrinth, L, C);
-       /* result = choose_test(test_mode, labyrinth, L, C, a, b, c, d, traversed_path, size_traversed);
+        result = choose_test(test_mode, labyrinth, L, C, a, b, c, d);
         write_output_file(name, result, filename);
-        free_labyrinth(labyrinth, L, C);*/
-        result = choose_test(test_mode, labyrinth, L, C, a, b);
-        write_output(fp_out, result);
-        free_labyrinth(labyrinth, C);
+        free_labyrinth(labyrinth, L, C);
         count = 0;
     }
-    free(out_name);
     fclose(fp_in);
-    fclose(fp_out);
-    return;
+    free(test_mode);
+    return labyrinth;
 }
 
-void free_labyrinth(int **labyrinth, int C)
+void free_labyrinth(int **labyrinth, int L, int C)
 {
     int i;
     for (i = 0; i < C; i++)
@@ -69,48 +85,4 @@ void free_labyrinth(int **labyrinth, int C)
     }
     free(labyrinth);
     return;
-}
-
-char *allocate_outputname(char *out_name, char *in_name)
-{
-    int num_char = 0;
-    num_char = find_last_period(in_name);
-    out_name = (char *)calloc((num_char + 5), sizeof(char));
-    if (out_name == NULL)
-    {
-        fprintf(stderr, "Not enough memory available!\n");
-        exit(5);
-    }
-    strncpy(out_name, in_name, num_char);
-    strcat(out_name, "sol1");
-
-    return out_name;
-}
-
-void error(FILE *fp)
-{
-    printf("Error when reading the input file.\n");
-    fclose(fp);
-    exit(0);
-}
-
-FILE *open_inputfile(FILE *fp_in, char *filename)
-{
-    if ((fp_in = fopen(filename, "r")) == NULL)
-    {
-        printf("Error when reading the input file.\n");
-        exit(1);
-    }
-    return fp_in;
-}
-
-FILE *open_outputfile(FILE *fp_out, char *filename)
-{
-    if ((fp_out = fopen(filename, "a")) == NULL)
-    {
-        printf("Error when writing the output file.\n");
-        free(filename);
-        exit(1);
-    }
-    return fp_out;
 }
